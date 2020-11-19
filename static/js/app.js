094bf2c99660f5488b3e165cc5cd3cb2b36bed76
bradhika80 <bradhika80@gmail.com>
2020-11-19 13:40:02 -0600

    retrieve data from data.json

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,37 @@
 
+// initialize variables
+var testSubjects; 
+var metadata;
+var samples;
 
-// populate options for the test Subjects
-PopulateTestSubjectOptions();
+// A function to do the initialization before the page loads
+function PreLoad()
+{   // read the json and build data variables
+    d3.json("data/samples.json").then((data) => {
+        testSubjects = data.names;
+        metadata = data.metadata;
+        samples = data.samples;
+        console.log (samples);
+        // populate options for the test Subjects
+      PopulateTestSubjectOptions();
+
+    });
+
+     
+}
 
 
 //function to prepopulate TestSubject Options
 function PopulateTestSubjectOptions(){
-    
-   // read the names field for the test subject
-    d3.json("data/samples.json").then((data) => {
-       
-       console.log(data.names)
+
        var options = d3.select("#selDataset")
                     .selectAll('option')
-                    .data(data.names)
+                    .data(testSubjects)
                     .enter()
                     .append('option')
-                    .text(function (d) { return d; });
-        });
-
-    
-    
+                    .text(function (d) { return d; });    
 
 }
 
+// call the PreLoad function
+PreLoad()
