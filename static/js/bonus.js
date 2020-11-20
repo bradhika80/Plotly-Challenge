c7a9daa68ecdcd830f06eae8b7c4430c0c8ea900
bradhika80 <bradhika80@gmail.com>
2020-11-19 22:27:51 -0600

    update bonus chart

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -41,12 +41,10 @@ function gaugePointer(value){
 	
     var degrees = 180 -  ((value * 180)/10), radius = .5;
     
-    // calculate x and y values 
+    // calculate x and y values  - https://xaktly.com/DerivativesOfPolarFunctions.html
     var radians = degrees * Math.PI / 180;
     var x = radius * Math.cos(radians);
     var y = radius * Math.sin(radians);
-
-    console.log(degrees);
 
     // Path: may have to change to create a better triangle
     var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
@@ -54,67 +52,61 @@ function gaugePointer(value){
         space = ' ',
         pathY = String(y),
         pathEnd = ' Z';
-    var path = mainPath.concat(pathX,space,pathY,pathEnd);
-        
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);        
 	return path;
-
 }
 
 
 //A function to build gauge chart - There is no direct way to create the guage chart.
 // Reference  :- https://codepen.io/ascotto/pen/eGNaqe?editors=0011
-function BuildC(washFrequency)
+function BuildChart(washFrequency)
 {
     console.log(washFrequency)
 
- // convert the value to float
-  var level = parseFloat(washFrequency);
+    // convert the value to float
+    var level = parseFloat(washFrequency);
 
 
-  // build a scatter plot
+    // build a scatter plot
 
-var data = [{ type: 'scatter',
-    x: [0], y:[0],	
-	showlegend: false,
-	name: 'scrub washing',
-	text: level,
-	hoverinfo: 'text+name'},
-    { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9 , 50], // values for the chart, 50% sliced into 8, and remaining 50%
-    rotation: 90,
-    text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1",""], // labels for the 
-    textinfo: 'text',
-    textposition:'inside',	  
-    marker: {colors:["#023002","#046904","#057c05" , "#08da08" ,"#72fa72" , "#4cf84c","#98fb98" , "#befcbe", "#e4fee4", "white"]},
-    labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1",""],
-    hoverinfo: 'label',
-    hole: .5,
-    type: 'pie',
-    showlegend: false
-    }];
+    var data = [{ type: 'scatter',
+        x: [0], y:[0],	
+        showlegend: false,
+        name: 'scrub washing',
+        text: level,
+        hoverinfo: 'text+name'},
+        { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9 , 50], // values for the chart, 50% sliced into 8, and remaining 50%
+        rotation: 90,
+        text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1",""], // labels for the 
+        textinfo: 'text',
+        textposition:'inside',	  
+        marker: {colors:["#023002","#046904","#057c05" , "#08da08" ,"#72fa72" , "#4cf84c","#98fb98" , "#befcbe", "#e4fee4", "white"]},
+        labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1",""],
+        hoverinfo: 'label',
+        hole: .5,
+        type: 'pie',
+        showlegend: false
+        }];
 
-var layout = {
-  shapes:[{
-      type: 'path',
-      path: gaugePointer(level),
-      fillcolor: '850000',
-      line: {
-        color: '850000'
-      }
-    }],
-  //title: '<b>Gauge</b> <br> Speed 0-100',
-	autosize:true,
-  //height: 1000,
-  //width: 1000,
-  xaxis: {zeroline:false, showticklabels:false,
-			 showgrid: false, range: [-1, 1]},
-  yaxis: {zeroline:false, showticklabels:false,
-			 showgrid: false, range: [-1, 1]}
-};
+    var layout = {
+    shapes:[{
+        type: 'path',
+        path: gaugePointer(level),
+        fillcolor: '850000',
+        line: {
+            color: '850000'
+        }
+        }],
+    //title: '<b>Gauge</b> <br> Speed 0-100',
+        autosize:true,
+    //height: 1000,
+    //width: 1000,
+    xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+    };
 
-Plotly.newPlot('gauge', data, layout);
-
-
-
-
+    Plotly.newPlot('gauge', data, layout);
 
 }
